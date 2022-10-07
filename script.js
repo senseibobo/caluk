a94c8506cdeb107458ff02bbadce30fe65a0cc31
senseibobo <bogdanjsd@gmail.com>
2022-10-07 18:49:18 +0200

    a
    a

--- script.js
+++ script.js
@@ -19,20 +19,22 @@ function kliknut(n) {
 function check(val) {
   let p = document.getElementById("poruka");
   if (val == "1993") {
-    document.getElementById("connor").style.visibility = "visible";
-    document.getElementById("connor").style.display = "block";
-    document.getElementById("yt").style.visibility = "hidden";
-    document.getElementById("poruka").innerText = "";
+    reveal("connor");
   } else if (val == "129857") {
-    document.getElementById("connor").style.visibility = "hidden";
-    document.getElementById("connor").style.display = "none";
-    document.getElementById("yt").style.visibility = "visible";
-    document.getElementById("poruka").innerText = "";
+    reveal("yt");
   } else if (val == "54098") {
-    document.getElementById("connor").style.visibility = "hidden";
-    document.getElementById("connor").style.display = "none";
-    document.getElementById("yt").style.visibility = "hidden";
-    document.getElementById("poruka").innerHTML =
-      "<h1><a href='https://www.google.com/maps/@44.6610863,20.9334678,3a,40.6y,228.63h,62.68t/data=!3m6!1e1!3m4!1smc5qwjoGyYpUFVK0qXvFmQ!2e0!7i13312!8i6656'>link</a>";
+    reveal("maps");
+  }
+}
+
+function reveal(id) {
+  for(let b of document.getElementById("stvari").children) {
+    if(b.id == id) {
+      b.style.visibility = "visible";
+      b.style.display = "block";
+    } else {
+      b.style.visibility = "hidden";
+      b.style.display = "none";
+    }
   }
 }
